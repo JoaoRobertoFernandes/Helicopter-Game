207ebbb78c0bb1c29280e5162d114d4f78f35f94
JoaoFernandes <joaofernandes4891@gmail.com>
2020-12-17 10:26:41 -0300

    Start animations.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 /*----------Start----------*/
 function start() {
-    $(".start").hide();
-    $(".background").append("<div class='player'></div>");
-    $(".background").append("<div class='enemy1'></div>");
-    $(".background").append("<div class='enemy2'></div>");
-    $(".background").append("<div class='friend'></div>");
+    $("#start").hide();
+
+    $("#background").append("<div id='player' class='ani1'></div>");
+    $("#background").append("<div id='enemy1' class='ani2'></div>");
+    $("#background").append("<div id='enemy2' ></div>");    
+    $("#background").append("<div id='friend' class='ani3'></div>");
 }
 /*----------Start----------*/

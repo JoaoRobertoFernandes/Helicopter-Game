0bdef3c69147260dd5ca7dbd8561db17e6c15354
JoaoFernandes <joaofernandes4891@gmail.com>
2020-12-17 09:44:43 -0300

    Start game layout  and render spites..

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*----------Start----------*/
+function start() {
+    $(".start").hide();
+    $(".background").append("<div class='player'></div>");
+    $(".background").append("<div class='enemy1'></div>");
+    $(".background").append("<div class='enemy2'></div>");
+    $(".background").append("<div class='friend'></div>");
+}
+/*----------Start----------*/

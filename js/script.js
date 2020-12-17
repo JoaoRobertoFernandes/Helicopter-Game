7a47f7ecc9c24929bfe4c0b06bc24a118805bd8d
JoaoFernandes <joaofernandes4891@gmail.com>
2020-12-17 10:55:52 -0300

    Player movement.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 /*----------Variables----------*/
 var game = {};
+var k = {
+    w: 87,
+    s: 83,
+    d: 68
+}
 /*----------Variables----------*/
 
 /*----------Start----------*/
@@ -11,11 +16,22 @@ function start() {
     $("#background").append("<div id='enemy2' ></div>");    
     $("#background").append("<div id='friend' class='ani3'></div>");
 
+    /*----------Keypress----------*/
+    game.press = [];
+    $(document).keydown(function(e) {
+        game.press[e.which] = true;
+    });
+
+    $(document).keyup(function(e) {
+        game.press[e.which] = false;
+    });
+    /*----------Keypress----------*/
     
     /*----------Loop----------*/
     game.timer = setInterval(loop,30);
     function loop() {
         moveBg();
+        movePlayer();
     }
     /*----------Loop----------*/
 
@@ -26,5 +42,34 @@ function start() {
     }    
     /*----------Background Movement----------*/    
 
+    /*----------Player Movement----------*/  
+    function movePlayer() {
+        if (game.press[k.w]) {
+            var top = parseInt($("#player").css("top"));
+            $("#player").css("top",  top - 10);
+
+            /*-----Limit Position-----*/
+            if (top <= 0) {
+                $("#player").css("top",  top + 10);
+            } 
+            /*-----Limit Position-----*/ 
+        }
+
+        if (game.press[k.s]) {
+            var top = parseInt($("#player").css("top"));
+            $("#player").css("top",  top + 10);
+
+             /*-----Limit Position-----*/
+             if (top >= 437) {
+                $("#player").css("top",  top - 10);
+            } 
+            /*-----Limit Position-----*/ 
+        }
+
+        if (game.press[k.d]) {
+
+        }
+    }
+    /*----------Player Movement----------*/  
 }
 /*----------Start----------*/
